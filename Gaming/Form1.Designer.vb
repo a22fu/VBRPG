73073df093695bd38d6d41e596e03c8953dcc051
a22fu <97246649+a22fu@users.noreply.github.com>
2022-01-16 16:04:51 -0500

    Add files via upload

---
+++ Gaming/Form1.Designer.vb
@@ -0,0 +1,131 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.lblnpc1 = New System.Windows.Forms.Label()
+        Me.picnpc1 = New System.Windows.Forms.PictureBox()
+        Me.picPlayer = New System.Windows.Forms.PictureBox()
+        Me.picspeak = New System.Windows.Forms.PictureBox()
+        Me.pictxt = New System.Windows.Forms.PictureBox()
+        Me.PictureBox1 = New System.Windows.Forms.PictureBox()
+        CType(Me.picnpc1, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.picPlayer, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.picspeak, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.pictxt, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'lblnpc1
+        '
+        Me.lblnpc1.BackColor = System.Drawing.Color.Black
+        Me.lblnpc1.Font = New System.Drawing.Font("Microsoft Sans Serif", 26.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.lblnpc1.ForeColor = System.Drawing.Color.White
+        Me.lblnpc1.Location = New System.Drawing.Point(299, 396)
+        Me.lblnpc1.Name = "lblnpc1"
+        Me.lblnpc1.Size = New System.Drawing.Size(495, 130)
+        Me.lblnpc1.TabIndex = 2
+        Me.lblnpc1.Text = "Hello Big Bird 1"
+        Me.lblnpc1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter
+        Me.lblnpc1.Visible = False
+        '
+        'picnpc1
+        '
+        Me.picnpc1.Image = Global.Gaming.My.Resources.Resources.BigBird
+        Me.picnpc1.Location = New System.Drawing.Point(216, 201)
+        Me.picnpc1.Name = "picnpc1"
+        Me.picnpc1.Size = New System.Drawing.Size(60, 60)
+        Me.picnpc1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage
+        Me.picnpc1.TabIndex = 1
+        Me.picnpc1.TabStop = False
+        '
+        'picPlayer
+        '
+        Me.picPlayer.Image = Global.Gaming.My.Resources.Resources.BigBird
+        Me.picPlayer.Location = New System.Drawing.Point(12, 261)
+        Me.picPlayer.Name = "picPlayer"
+        Me.picPlayer.Size = New System.Drawing.Size(60, 60)
+        Me.picPlayer.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage
+        Me.picPlayer.TabIndex = 0
+        Me.picPlayer.TabStop = False
+        '
+        'picspeak
+        '
+        Me.picspeak.Location = New System.Drawing.Point(211, 196)
+        Me.picspeak.Name = "picspeak"
+        Me.picspeak.Size = New System.Drawing.Size(70, 70)
+        Me.picspeak.TabIndex = 3
+        Me.picspeak.TabStop = False
+        Me.picspeak.Visible = False
+        '
+        'pictxt
+        '
+        Me.pictxt.BackgroundImage = Global.Gaming.My.Resources.Resources.Textbox
+        Me.pictxt.Image = Global.Gaming.My.Resources.Resources.Textbox
+        Me.pictxt.Location = New System.Drawing.Point(270, 381)
+        Me.pictxt.Name = "pictxt"
+        Me.pictxt.Size = New System.Drawing.Size(556, 162)
+        Me.pictxt.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage
+        Me.pictxt.TabIndex = 4
+        Me.pictxt.TabStop = False
+        Me.pictxt.Visible = False
+        '
+        'PictureBox1
+        '
+        Me.PictureBox1.Image = Global.Gaming.My.Resources.Resources.TownBack
+        Me.PictureBox1.Location = New System.Drawing.Point(-3, -1)
+        Me.PictureBox1.Name = "PictureBox1"
+        Me.PictureBox1.Size = New System.Drawing.Size(1090, 564)
+        Me.PictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage
+        Me.PictureBox1.TabIndex = 5
+        Me.PictureBox1.TabStop = False
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch
+        Me.ClientSize = New System.Drawing.Size(1084, 561)
+        Me.Controls.Add(Me.lblnpc1)
+        Me.Controls.Add(Me.picnpc1)
+        Me.Controls.Add(Me.picPlayer)
+        Me.Controls.Add(Me.picspeak)
+        Me.Controls.Add(Me.pictxt)
+        Me.Controls.Add(Me.PictureBox1)
+        Me.Name = "Form1"
+        Me.Text = "BingBong Gaming"
+        CType(Me.picnpc1, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.picPlayer, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.picspeak, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.pictxt, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+
+    End Sub
+
+    Friend WithEvents picPlayer As PictureBox
+    Friend WithEvents picnpc1 As PictureBox
+    Friend WithEvents lblnpc1 As Label
+    Friend WithEvents picspeak As PictureBox
+    Friend WithEvents pictxt As PictureBox
+    Friend WithEvents PictureBox1 As PictureBox
+End Class
---
+++ Gaming/Form1.vb
@@ -0,0 +1,100 @@
+﻿Public Class Form1
+
+    Dim loc As Point   'location of player
+
+    Dim moving As Boolean = True   'Check to allow movement, true = can move
+
+    Dim Chat As Integer = 0    'Integer to check how many text boxes the npc has spoken
+
+
+    Private Sub Form1_KeyDown(sender As Object, e As KeyEventArgs) Handles Me.KeyDown
+        Dim collision(4) As PictureBox       'array for all the picture boxes in the form
+        collision = {picnpc1}
+
+        Select Case e.KeyCode    'key
+
+            Case Keys.D     'D is the move right, W is move up, A is move left, S is move down
+                'The movement is controlled by moving the character by 5 in the direction first and then checking if moving is false, 
+                'if it intersects with a Picture box or if it goes off screen it will move the player back. This gives the illusion that there was no movement  
+
+                For Each P As PictureBox In collision
+                    loc = New Point(picPlayer.Location.X + 5, picPlayer.Location.Y)
+                    picPlayer.Location = loc
+                    If picPlayer.Location.X > 1025 Or moving = False Or picPlayer.Bounds.IntersectsWith(P.Bounds) = True Then      'repeat for all directions
+                        loc = New Point(picPlayer.Location.X - 5, picPlayer.Location.Y)
+                        picPlayer.Location = loc
+                    End If
+                Next
+
+            Case Keys.A
+
+
+                For Each P As PictureBox In collision
+                    loc = New Point(picPlayer.Location.X - 5, picPlayer.Location.Y)
+                    picPlayer.Location = loc
+                    If picPlayer.Location.X < 0 Or moving = False Or picPlayer.Bounds.IntersectsWith(P.Bounds) = True Then      'repeat for all directions
+                        loc = New Point(picPlayer.Location.X + 5, picPlayer.Location.Y)
+                        picPlayer.Location = loc
+                    End If
+                Next
+
+            Case Keys.W
+
+
+                For Each P As PictureBox In collision
+                    loc = New Point(picPlayer.Location.X, picPlayer.Location.Y - 5)
+                    picPlayer.Location = loc
+                    If picPlayer.Location.Y < 0 Or moving = False Or picPlayer.Bounds.IntersectsWith(P.Bounds) = True Then      'repeat for all directions
+                        loc = New Point(picPlayer.Location.X, picPlayer.Location.Y + 5)
+                        picPlayer.Location = loc
+                    End If
+                Next
+
+            Case Keys.S
+
+                For Each P As PictureBox In collision
+                    loc = New Point(picPlayer.Location.X, picPlayer.Location.Y + 5)
+                    picPlayer.Location = loc
+                    If picPlayer.Location.Y > 500 Or moving = False Or picPlayer.Bounds.IntersectsWith(P.Bounds) = True Then      'repeat for all directions
+                        loc = New Point(picPlayer.Location.X, picPlayer.Location.Y - 5)
+                        picPlayer.Location = loc
+                    End If
+                Next
+
+
+            Case Keys.Space
+                If picPlayer.Bounds.IntersectsWith(picspeak.Bounds) And Chat = 0 Then     'If the player is in the hitbox for the npc
+                    Me.lblnpc1.Visible = True
+                    pictxt.Visible = True
+                    moving = False
+                    Chat = 1
+                ElseIf Chat = 1 Then
+                    lblnpc1.Text = "You must defeat the monsters to continue"
+                    Chat = 2
+                ElseIf Chat = 2 Then
+                    lblnpc1.Text = "Go now"
+                    Chat = 3
+                ElseIf Chat = 3 Then
+                    lblnpc1.Visible = False
+                    moving = True
+                    Chat = 4
+                    pictxt.Visible = False
+                ElseIf Chat = 4 And picPlayer.Bounds.IntersectsWith(picspeak.Bounds) Then
+                    lblnpc1.Visible = True
+                    lblnpc1.Text = "Go"
+                    Chat = 5
+                    pictxt.Visible = True
+                    moving = False
+                ElseIf Chat = 5 Then
+                    lblnpc1.Visible = False
+                    Chat = 4
+                    pictxt.Visible = False
+                    moving = True
+                End If
+        End Select
+
+
+    End Sub
+
+
+End Class
